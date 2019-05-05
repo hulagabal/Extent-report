c12899f5eb48196f4e19c9307f12b7d51e16c718
Mutturaj Hulagabal <hulagabal@gmail.com>
2019-05-05 11:26:54 +0530

    added log4j2 implementation files

--- src/test/java/extentReportForMultipleTestsInAClass/ClassA.java
+++ src/test/java/extentReportForMultipleTestsInAClass/ClassA.java
@@ -1,28 +1,35 @@
 package extentReportForMultipleTestsInAClass;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ClassA extends BaseTest {
+	private static final Logger log=LogManager.getLogger(ClassA.class);
 
 	@Test
 	public void method1() {
-		test = extent.createTest("class A1");
+		log.info("Method 1 started");
+		test = extent.createTest("method1");
 
 		driver.get("https://www.facebook.com/");
 		String title = driver.getTitle();
 		System.out.println(title);
+		log.info(title);
 		Assert.assertEquals(title, "Facebook – log");
 
 	}
 
 	@Test
 	public void method2() {
-		test = extent.createTest("class A1");
+		log.info("Method 2 started");
+		test = extent.createTest("method1");
 
 		driver.get("https://www.facebook.com/");
 		String title = driver.getTitle();
 		System.out.println(title);
+		log.info(title);
 		Assert.assertEquals(title, "Facebook – log");
 
 	}
--- src/test/java/extentReportForMultipleTestsInAClass/ClassB.java
+++ src/test/java/extentReportForMultipleTestsInAClass/ClassB.java
@@ -1,18 +1,22 @@
 package extentReportForMultipleTestsInAClass;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ClassB extends BaseTest {
-
+	private static final Logger log=LogManager.getLogger(ClassB.class);
 	@Test
-	public void classB1() {
+	public void method1() {
+		log.info("Method 1 started");
 		test = extent.createTest("class B1");
 	
 		driver.get("https://www.facebook.com/");
 		String title=driver.getTitle();
 		System.out.println(title);
 		Assert.assertEquals(title, "Facebook – log");
+		log.info("test completede");
 		
 		
 		
